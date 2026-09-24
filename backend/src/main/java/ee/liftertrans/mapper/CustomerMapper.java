3fddf6ec537d7cf345459aae17987385eb9742e7
opilane_Karli <cours885@gmail.com>
2026-09-24 16:22:32 +0300

    LIF-56_1

--- backend/src/main/java/ee/liftertrans/mapper/CustomerMapper.java
+++ backend/src/main/java/ee/liftertrans/mapper/CustomerMapper.java
@@ -4,6 +4,7 @@ package ee.liftertrans.mapper;
 import ee.liftertrans.dto.CustomerListDto;
 import ee.liftertrans.persistence.entity.Customer;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
@@ -14,7 +15,24 @@ import java.util.List;
 )
 public interface CustomerMapper {
 
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "companyName", target = "companyName")
+    @Mapping(source = "companyRegistrationNumber", target = "companyRegistrationNumber")
+    @Mapping(source = "vatNumber", target = "vatNumber")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "invoiceEmail", target = "invoiceEmail")
+    @Mapping(source = "phone", target = "phone")
+
     CustomerListDto toCustomerDto(Customer customer);
 
     List<CustomerListDto> toCustomerDtos(List<Customer> customers);
 }
+private Integer id;
+private String name;
+private String companyName;
+private String companyRegistrationNumber;
+private String vatNumber;
+private String email;
+private String invoiceEmail;
+private String phone;
