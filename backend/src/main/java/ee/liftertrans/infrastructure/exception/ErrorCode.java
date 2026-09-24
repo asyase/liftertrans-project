dbbfa0179237690a63fb0b6eda6780807ae029c4
opilane_Karli <cours885@gmail.com>
2026-09-24 16:04:39 +0300

    LIF-56_1

--- backend/src/main/java/ee/liftertrans/infrastructure/exception/ErrorCode.java
+++ backend/src/main/java/ee/liftertrans/infrastructure/exception/ErrorCode.java
@@ -5,15 +5,15 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-    INVALID_SEARCH_PARAMETER(HttpStatus.BAD_REQUEST, "Otsingu parameeter on vigane"),
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "Kasutaja ei ole sisse logitud"),
-    ACCESS_DENIED(HttpStatus.FORBIDDEN, "Kasutajal puudub ligipääs");
+  INVALID_SEARCH_PARAMETER(HttpStatus.BAD_REQUEST, "Otsingu parameeter on vigane"),
+  UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "Kasutaja ei ole sisse logitud"),
+  ACCESS_DENIED(HttpStatus.FORBIDDEN, "Kasutajal puudub ligipääs");
 
-    private final HttpStatus status;
-    private final String message;
+  private final HttpStatus status;
+  private final String message;
 
-    ErrorCode(HttpStatus status, String message) {
-        this.status = status;
-        this.message = message;
-    }
+  ErrorCode(HttpStatus status, String message) {
+    this.status = status;
+    this.message = message;
+  }
 }
